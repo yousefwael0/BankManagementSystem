0a5b84dc3b19980235b3fad1f9e7461be96dbe1d
Karim amr <karimamr20055@gmail.com>
2024-12-15 04:54:12 +0200

    transaction changed

--- src/main/models/account/Transaction.java
+++ src/main/models/account/Transaction.java
@@ -1,51 +1,76 @@
 package models.account;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Transaction {
-    private String transactionID;
+    private long transactionId;
+    private LocalDateTime date;
+    private String type;
     private double amount;
-    private String type; // Deposit, Withdrawal, Transfer
-    private Date date;
-    private String clientID;
-    private String empID;
-
-    public Transaction(String transactionID, double amount, String type, Date date, String clientID, String empID) {
-        this.transactionID = transactionID;
-        this.amount = amount;
-        this.type = type;
+    private String employeeId;
+    private int counter=1;
+    // Constructor
+    public Transaction(long transactionId, LocalDateTime date, String type, double amount, String employeeId) {
+        this.transactionId = counter;
         this.date = date;
-        this.clientID = clientID;
+        this.type = type;
+        this.amount = amount;
+        this.employeeId = employeeId;
+        counter++;
     }
 
-    public String getTransactionID() {
-        return transactionID;
+    // Getter and Setter methods
+    public long getTransactionId() {
+        return transactionId;
     }
 
-    public double getAmount() {
-        return amount;
+    public void setTransactionId(long transactionId) {
+        this.transactionId = counter;
+        counter++;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 
     public String getType() {
         return type;
     }
 
-    public Date getDate() {
-        return date;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getClientID() {
-        return clientID;
+    public double getAmount() {
+        return amount;
     }
 
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "transactionID='" + transactionID + '\'' +
-                ", amount=" + amount +
-                ", type='" + type + '\'' +
-                ", date=" + date +
-                ", clientID='" + clientID + '\'' +
-                '}';
+    public void setAmount(double amount) {
+        this.amount = amount;
     }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+
+    // Method to get transaction details
+    public String getTransactionDetails() {
+        return "Transaction Details:\n" +
+                "Transaction ID: " + transactionId + "\n" +
+                "Date: " + date + "\n" +
+                "Type: " + type + "\n" +
+                "Amount: " + amount + "\n" +
+                "Employee ID: " + employeeId;
+    }
+
 }
+
